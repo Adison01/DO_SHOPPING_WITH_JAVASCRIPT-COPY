3a66136c233f041c29700c85b3a5cf209a5dd008
TaherSalauddin <tahersalauddin@gmail.com>
2024-01-13 09:17:04 -0600

    new code

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log("aaaaa");
+const cart = [];
+const createTable = (data) => {
+  const price = data.parentNode.parentNode.children[1].children[0].innerText;
+  const pdName = data.parentNode.parentNode.children[0].innerText;
+  const pdNameAndPrice = {
+    price: parseInt(price),
+    pdName,
+  };
+  cart.push(pdNameAndPrice);
+  displayTable();
+};
+
+function displayTable() {
+  const productNo = document.getElementById("total-products");
+  productNo.innerText = cart.length;
+  const mainContainer = document.getElementById("products-cart");
+  document.getElementById("products-cart").textContent = "";
+  let totalPrice = 0;
+  for (let i = 0; i < cart.length; i++) {
+    totalPrice = totalPrice + cart[i].price;
+    const createRow = document.createElement("tr");
+
+    createRow.innerHTML = `
+    <th>${i + 1}</th>
+    <td>${cart[i].pdName}</td>
+    <td>${cart[i].price}</td>
+    `;
+    mainContainer.appendChild(createRow);
+  }
+  const forTotalPrice = document.createElement("tr");
+  forTotalPrice.innerHTML = `
+  <th></th>
+  <td>Total Price : </td>
+  <td>${totalPrice}</td>
+  `;
+  mainContainer.appendChild(forTotalPrice);
+}
+
+const forTest = () => {
+  console.log("adfasdf");
+};
